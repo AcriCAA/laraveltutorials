46809e3e6afad31d632464aa6afde5b95fa080db
Corey Acri <acri.corey@gmail.com>
2017-10-27 18:37:01 -0400

    pulling in dynamic data --- adding foreach to go through posts

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -5,7 +5,7 @@
             <h2 class="blog-post-title">{{$post->title}}</h2>
         </a>
             {{-- The date string is using the Carbon library that is built in Laravel --}}
-            <p class="blog-post-meta">{{$post->created_ad->toFormattedDateString()}}<a href="#">Mark</a></p>
+            <p class="blog-post-meta">{{$post->created_at->toFormattedDateString()}}<a href="#">Mark</a></p>
 
             <p>{{$post->body}}</p>
 
