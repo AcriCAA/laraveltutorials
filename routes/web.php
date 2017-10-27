ad16d6cb612246b842bcf92738d5af8db9784fae
Corey Acri <acri.corey@gmail.com>
2017-10-27 16:48:13 -0400

    updating route for create

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('/', 'PostsController@index'); 
 
 // Route::get('/posts/{post}', 'PostsController@show'); 
-Route::get('/posts/create', 'PostsController@show'); 
+Route::get('/posts/create', 'PostsController@create'); 
 
 Route::post('/posts', 'PostsController@store'); 
 
