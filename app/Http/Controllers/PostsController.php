5afa45a5d4d33b6c75c9007af4ba29137c88793c
Corey Acri <acri.corey@gmail.com>
2017-10-27 18:13:44 -0400

    validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -33,24 +33,30 @@ class PostsController extends Controller
 	}
 
 
-	public function store(){
+	public function store()
+
+	{
+
+		$this->validate(request(), [
+
+			'title' => 'required|max:20', 
+			'body' => 'required'
+
+		]);
+		
 
 		//Creates a new post and requests the array passed in and saves it to the db
 		//IMPORTANT: BE EXPLICIT! only pass the fields you are comfortable submitted to the server
 		Post::create(request(['title','body']));
-		//^^^ that does the same as this:
+		/*
 
-
-		// $post = new Post; 
-		// //Create a new post using the request data
-
-		// $post->title = request('title'); 
-
-		// $post->body = request('body'); 
-
-
-		// $post->save(); 
-		// // Save it to the database
+		^^^ that does the same as this:
+		$post = new Post; 
+		//Create a new post using the request data
+		$post->title = request('title'); 
+		$post->body = request('body'); 
+		$post->save(); 
+		// Save it to the database */
 
 		// And then redirect to the homepage
 		return redirect('/');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,18 +13,35 @@
 
         	<h1>Publish a Post</h1>
 
+
+@if(count($errors))
+
+<div class="row">
+	<div class="col-sm-12 alert-danger">
+
+		<ul>
+			@foreach($errors->all() as $error)
+				<li>{{$error}}</li>
+			@endforeach
+		</ul>
+
+	</div>
+</div>
+
+@endif
+
 <form method="POST" action="/posts">
 {{-- include csrf field in all of our forms for authentication --}}
 	{{ csrf_field() }}
   <div class="form-group">
     <label for="title">Title: </label>
-    <input type="text" class="form-control" id="title" name="title">
+    <input type="text" class="form-control" id="title" name="title" required >
   </div>
 
 
   <div class="form-group">
     <label for="body">Body</label>
-    <textarea type="text" class="form-control" id="body" name="body"></textarea>
+    <textarea type="text" class="form-control" id="body" name="body" required ></textarea>
   </div>
 
 
