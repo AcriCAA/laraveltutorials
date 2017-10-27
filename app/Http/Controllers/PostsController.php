4b590792111daada90bbf1522b19398c8d8be06d
Corey Acri <acri.corey@gmail.com>
2017-10-27 16:13:50 -0400

    fixing

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -19,25 +19,11 @@ class PostsController extends Controller
 
 
 
- public function show(){ // by putting Post $post in here and matching it to the wildcard {$task in routes} (Route::get('/tasks/{task}', 'TasksController@show');), Laravel is automatically running Task::find($id) or whatever wildcard 
+	public function show(){ 
 
 
-	//db call to find a row by id, returns the entire record as object
-// $task = DB::table('tasks')->find($id); 
+		return view('posts.show');
 
-
-// this is no longer needed because of the Task $task model binding above
-// $task = Task::find($id); 
-
-
-// return $tasks;
-
-//dd= helper function for dump and die
-// dd($tasks);
-
-
-//refers to the route folder and then the blade file name before .blade.php , compact creates the necessary array for the $tasks variable
-return view('posts.show');
-
-}
+	}
+	
 }
