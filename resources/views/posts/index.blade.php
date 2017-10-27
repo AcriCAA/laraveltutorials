a71bd04d831487a1a89df9f60ec46c690f8b7990
Corey Acri <acri.corey@gmail.com>
2017-10-27 18:33:54 -0400

    pulling in dynamic data --- adding foreach to go through posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,7 +10,10 @@
 
         <div class="col-sm-8 blog-main">
 
+
+        @foreach($posts as $post)
          @include('posts.post')
+        @endforeach
 
           <nav class="blog-pagination">
             <a class="btn btn-outline-primary" href="#">Older</a>
