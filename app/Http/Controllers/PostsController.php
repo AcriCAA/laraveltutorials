08749672079ac7fe577bf56c54ffbb104eaa69ff
Corey Acri <acri.corey@gmail.com>
2017-10-29 15:36:34 -0400

    changing code in store to test

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -48,16 +48,16 @@ class PostsController extends Controller
 
 		//Creates a new post and requests the array passed in and saves it to the db
 		//IMPORTANT: BE EXPLICIT! only pass the fields you are comfortable submitted to the server
-		Post::create(request(['title','body']));
-		/*
+		// Post::create(request(['title','body']));
+		
 
-		^^^ that does the same as this:
+		// ^^^ that does the same as this:
 		$post = new Post; 
 		//Create a new post using the request data
 		$post->title = request('title'); 
 		$post->body = request('body'); 
 		$post->save(); 
-		// Save it to the database */
+		// Save it to the database 
 
 		// And then redirect to the homepage
 		return redirect('/');
