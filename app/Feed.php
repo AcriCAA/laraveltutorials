0ca319eda68a970e0d9b993f3a331a77df5f5725
Corey Acri <acri.corey@gmail.com>
2017-12-14 16:45:54 -0500

    updates

--- app/Feed.php
+++ app/Feed.php
@@ -7,36 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
     //
-    	public function saveApiData()
-    {
-        $client = new Client();
-
-
-       // $response = $client->request('GET', 'search?query=driverless');
-
-
-
-
-        // $res = $client->request('POST', 'https://backend.theedge.ai/api/v2/links/search?query=driverless', [
-        //     'form_params' => [
-        //         'client_id' => 'test_id',
-        //         'secret' => 'test_secret',
-        //     ]
-        // ]);
-
-       $response = $client->get('https://backend.theedge.ai/api/v2/links/search?query=driverless');
-		// $response = $client->delete('http://httpbin.org/delete');
-		// $response = $client->head('http://httpbin.org/get');
-		// $response = $client->options('http://httpbin.org/get');
-		// $response = $client->patch('http://httpbin.org/patch');
-		// $response = $client->post('http://httpbin.org/post');
-		// $response = $client->put('http://httpbin.org/put');
-       
-       	$result = json_decode($response->getBody()->getContents());
-
-       	return $result; 
-        
-
-	}
+  
 
 }
--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -9,13 +9,43 @@ use GuzzleHttp\Client;
 class FeedController extends Controller
 {
     //
+      public function saveApiData()
+    {
+        $client = new Client();
 
+
+       // $response = $client->request('GET', 'search?query=driverless');
+
+
+
+
+        // $res = $client->request('POST', 'https://backend.theedge.ai/api/v2/links/search?query=driverless', [
+        //     'form_params' => [
+        //         'client_id' => 'test_id',
+        //         'secret' => 'test_secret',
+        //     ]
+        // ]);
+
+       $response = $client->get('https://backend.theedge.ai/api/v2/links/search?query=driverless');
+    // $response = $client->delete('http://httpbin.org/delete');
+    // $response = $client->head('http://httpbin.org/get');
+    // $response = $client->options('http://httpbin.org/get');
+    // $response = $client->patch('http://httpbin.org/patch');
+    // $response = $client->post('http://httpbin.org/post');
+    // $response = $client->put('http://httpbin.org/put');
+       
+        $result = json_decode($response->getBody()->getContents());
+
+        echo $result; 
+        
+
+  }
 
 	public function show(){ 
 
-		$articles = saveApiData(); 
+		$articles = FeedController::saveApiData(); 
 
-		echo view('feeds.feedhome', compact('articles'));
+		return view('feeds.feedhome', compact('articles'));
 
 	}
 
