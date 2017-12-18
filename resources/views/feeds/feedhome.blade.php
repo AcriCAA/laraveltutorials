32cf32f16f06b0fa8bc6eada6ee869648bc2f360
Corey Acri <acri.corey@gmail.com>
2017-12-18 16:25:05 -0500

    fixing feed

--- resources/views/feeds/feedhome.blade.php
+++ resources/views/feeds/feedhome.blade.php
@@ -6,20 +6,15 @@
 
       <main role="main" class="container">
 
-      <div class="row">
-
-        <div class="col-sm-12 blue-bg">
+       
         @foreach($articles['data'] as $article)
-        <div class="row">
-          <div class="col-sm-12 pad-30">
-         <h2>{{$article['attributes']['title']}}</h2>
-          <p>{{$article['attributes']['description']}}</p>
-  <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
-  
-
-
-
-       </div> {{-- row --}}
+            <div class="row">
+              <div class="col-sm-12 pad-30 blue-bg">
+             <h2>{{$article['attributes']['title']}}</h2>
+              <p>{{$article['attributes']['description']}}</p>
+              <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+            </div>
+            </div> {{-- row --}}
         @endforeach
 
 
