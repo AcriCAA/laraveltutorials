e09b8835bb299847cf521eee35d0a261410e460f
Corey Acri <acri.corey@gmail.com>
2018-01-31 15:59:32 -0500

    feed into album

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -1,9 +1,48 @@
-@extends('layouts.layout')
+@extends('layouts.master')
+
 
 @section('content')
 
-<div class="container">
-<h1>A place to show the posts</h1>
-</div>
+{{$column_divisor = 1%3}}
+
+        <div class="album text-muted">
+        <div class="container">
+
+       
+
+        @foreach($articles['data'] as $article)
+
+
+        @if($loop->index % 3 = $column_divisor)
+        	<div class="row">
+        @endif
+
+        @if($loop->index % 3 == 0)
+     
+                   
+              <div class="card">
+                 <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
+                 <h2>{{$article['attributes']['title']}}</h2>
+                  <p class="card-text">{{$article['attributes']['description']}}</p>
+                  <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+            </div>
+        </div> {{-- close the row --}}    
+        
+        @else
+          <div class="card">
+               <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
+               <h2>{{$article['attributes']['title']}}</h2>
+                <p class="card-text">{{$article['attributes']['description']}}</p>
+                <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+            </div>
+        @endif
+        @endforeach
+
+		</div> {{-- album text muted --}}
+	
+	</div> {{-- container --}}
+
+    
 
 @endsection
+
