b5210a8e793cd230c46a4a6cc01917c2f9655fbe
Corey Acri <acri.corey@gmail.com>
2018-01-31 17:40:36 -0500

    feed

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -11,10 +11,18 @@
         {{-- {{var_dump($loop->index)}} --}}
         <?php $column_calculation = $loop->index % 3 ?>
 
-        {{$loop->index}}
+        @if($loop->index == 0)
+
+       	<div class="row">
+        		<div class="card">
+                 <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
+                 <h2>{{$article['attributes']['title']}}</h2>
+                  <p class="card-text">{{$article['attributes']['description']}}</p>
+                  <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+            </div>
 
 
-        @if ($column_calculation === $column_divisor)
+        @elseif ($column_calculation === $column_divisor)
         	<div class="row">
         		<div class="card">
                  <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
