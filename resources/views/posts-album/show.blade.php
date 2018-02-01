d1d6abf94cf3bbb49a2ad5eba3338715dc381756
Corey Acri <acri.corey@gmail.com>
2018-02-01 17:32:07 -0500

    updating columns

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -30,9 +30,9 @@
 
         @if ($column_calculation === $column_divisor)
         	<div class="mt-3 d-flex flex-row">
-        		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        		<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="border: 2px solid rgb(134, 142, 150);>
         	
-        		<div class="mr-1 ml-1 p-2" style="border: 2px solid rgb(134, 142, 150);">
+        		<div class="mr-1 ml-1 p-2">
                 <img class="card-img-top" src="{{ URL::to($article['attributes']['thumbnail']) }}"/>
                  <h2>{{$article['attributes']['title']}}</h2>
                   <p class="card-text">{{$article['attributes']['description']}}</p>
@@ -45,9 +45,9 @@
         {{-- middle column --}}
         @elseif($odd_calculation != 0)
           
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="border: 2px solid rgb(134, 142, 150);>
           
-        	<div class="mr-2 ml-2 p-3" style="border: 2px solid rgb(134, 142, 150);">
+        	<div class="mr-2 ml-2 p-3">
               <img class="card-img-top" src="{{ URL::to($article['attributes']['thumbnail']) }}"/>
                <h2>{{$article['attributes']['title']}}</h2>
                 <p class="card-text">{{$article['attributes']['description']}}</p>
@@ -57,9 +57,9 @@
 
         {{-- last third column --}}
         @elseif($odd_calculation === 0)
-     			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+     			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4" style="border: 2px solid rgb(134, 142, 150);>
               
-              	<div class="mr-2 ml-2 p-3" style="border: 2px solid rgb(134, 142, 150);">
+              	<div class="mr-2 ml-2 p-3">
          			<img class="card-img-top" src="{{ URL::to($article['attributes']['thumbnail']) }}"/>
                 
                  <h2>{{$article['attributes']['title']}}</h2>
