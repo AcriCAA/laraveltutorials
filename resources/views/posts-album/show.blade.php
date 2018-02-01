c462caa9d051d893a78ff7c18210928db9b9b5d7
Corey Acri <acri.corey@gmail.com>
2018-02-01 00:31:15 -0500

    feed

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -34,7 +34,7 @@
                 <img class="card-img-top" src="{{ URL::to($article['attributes']['thumbnail']) }}"/>
                  <h2>{{$article['attributes']['title']}}</h2>
                   <p class="card-text">{{$article['attributes']['description']}}</p>
-                  <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+                  <p class="text-center"><button class="btn btn-primary"><a href="{{URL::to($article['attributes']['url'])}}">READ MORE</a></button></p>
             	</div>
         
 
@@ -48,7 +48,7 @@
               <img class="card-img-top" src="{{ URL::to($article['attributes']['thumbnail']) }}"/>
                <h2>{{$article['attributes']['title']}}</h2>
                 <p class="card-text">{{$article['attributes']['description']}}</p>
-                <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+                <p class="text-center"><button class="btn btn-primary"><a href="{{URL::to($article['attributes']['url'])}}">READ MORE</a></button></p>
             </div>
 
         {{-- last third column --}}
@@ -60,7 +60,7 @@
                 
                  <h2>{{$article['attributes']['title']}}</h2>
                   <p class="card-text">{{$article['attributes']['description']}}</p>
-                  <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
+                  <p class="text-center"><button class="btn btn-primary"><a href="{{URL::to($article['attributes']['url'])}}">READ MORE</a></button></p>
             </div>
         </div> {{-- close the row --}}    
         @endif
