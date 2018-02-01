18eb93bb65952b24e89d6695a95d8c96556c97ea
Corey Acri <acri.corey@gmail.com>
2018-02-01 00:22:12 -0500

    feed

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -1,7 +1,9 @@
 
 
-<?php $column_divisor = round((1/3),2);
-		?>
+		@php 
+		$column_divisor = round((1/3),2); 
+		@endphp
+
 
         <div class="album text-muted">
         <div class="container">
@@ -10,7 +12,7 @@
         @foreach($articles['data'] as $article)
 
         {{-- {{var_dump($loop->index)}} --}}
-        <?php 
+        @php
         $column_count = $loop->index + 1;
         $n = $column_count / 3; 
 		$whole = floor($n);      
@@ -20,7 +22,7 @@
 
 		$odd_calculation = $column_count % 3; 
 		
-        ?>
+        @endphp 
 
        
 
@@ -29,7 +31,7 @@
         @if ($column_calculation === $column_divisor)
         	<div class="row">
         		<div class="card">
-                {{ HTML::image('img/picture.jpg', 'a picture', array('class' => 'thumb')) }}
+                <img class="img-reponsive" src="{{url($articles['attributes']['thumbnail'])}}"
                  <h2>{{$article['attributes']['title']}}</h2>
                   <p class="card-text">{{$article['attributes']['description']}}</p>
                   <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
@@ -43,7 +45,7 @@
           <div class="card">
           		
       
-              {{ HTML::image('img/picture.jpg', 'a picture', array('class' => 'thumb')) }}
+              <img class="img-reponsive" src="{{url($articles['attributes']['thumbnail'])}}"
                <h2>{{$article['attributes']['title']}}</h2>
                 <p class="card-text">{{$article['attributes']['description']}}</p>
                 <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
