e5aa93469c29943e8a49c55018b4cba242bcf400
Corey Acri <acri.corey@gmail.com>
2018-01-31 23:50:26 -0500

    feed

--- resources/views/posts-album/show.blade.php
+++ resources/views/posts-album/show.blade.php
@@ -19,7 +19,7 @@ var_dump($column_divisor); ?>
 var_dump($column_calculation);
 
 		$odd_calculation = $column_count % 3; 
-		echo "Column Number=" .$column_count. " Column Calulation= ".$column_calculation." Odd Calculation=" . $odd_calculation; 
+		
         ?>
 
        
@@ -29,12 +29,12 @@ var_dump($column_calculation);
         @if ($column_calculation === $column_divisor)
         	<div class="row">
         		<div class="card">
-        			Case 1
+        			<?php echo "Column Number=" .$column_count. " Column Calulation= ".$column_calculation." Odd Calculation=" . $odd_calculation; ?>
                  <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
                  <h2>{{$article['attributes']['title']}}</h2>
                   <p class="card-text">{{$article['attributes']['description']}}</p>
                   <p class="text-center"><button><a href="{{$article['attributes']['url']}}">READ MORE</a></button></p>
-            </div>
+            	</div>
         
 
         {{-- last third column --}}
