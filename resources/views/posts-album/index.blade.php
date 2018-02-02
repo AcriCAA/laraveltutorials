bf250b022e06d9bad7439db3adb552318fd2f785
Corey Acri <acri.corey@gmail.com>
2018-02-02 08:40:49 -0500

    trying to add background image to jumbotron

--- resources/views/posts-album/index.blade.php
+++ resources/views/posts-album/index.blade.php
@@ -32,7 +32,7 @@
           <h1 class="jumbotron-heading white">Autonomous Vehicles</h1>
           <p class="lead white">The latest and most current news on autonomous vehicles, driving, and technology. You've been the Edged.</p>
           <p>
-          <p class="lead text-muted">Looking for more content like this?</p>
+          <p class="lead white">Looking for more content like this?</p>
           <p>
             <a href="http://www.theedge.group/" class="btn btn-primary">Contact Edge Group</a>
           </p>
