03d78f4fd3ac9914992d47f7fcd23de83ca908c5
Corey Acri <acri.corey@gmail.com>
2018-06-23 15:02:06 -0400

    echoing response

--- app/Http/Controllers/WorldCup.php
+++ app/Http/Controllers/WorldCup.php
@@ -25,7 +25,7 @@ public function wcapi(Request $request)
 $command = $request->input('command');
 $token = $request->input('token');
 $text = $request->input('text');
-// These are the new env variables to copy into Meetupcontroller to get today working with key
+
 
 $slack_token = config('services.slack.token');
 
@@ -50,7 +50,7 @@ echo $command;
 echo $text;
 echo $token; 
 
-return $response; 
+echo $response; 
 
 
 }
