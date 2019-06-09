ff7ed0ad3ddb4af06c02c799290cf142626dfc79
Corey Acri <acri.corey@gmail.com>
2019-06-09 08:59:46 -0400

    updaing logic and feed items

--- app/Http/Controllers/CSHLFeedController.php
+++ app/Http/Controllers/CSHLFeedController.php
@@ -17,14 +17,14 @@ public function test (Request $request) {
 
 public function logPost(Request $request){
 
-	\Log::info($request->getContents()); 
+	\Log::info($response->getBody()->getContents()); 
 }
 
 
 public function returnFeed(Request $request){
 
 	// \Log::info($request->getContents()); 
-	$postFields = $request->getContents(); 
+	$postFields = $response->getBody()->getContents();
 
 	\Log::info($postFields); 
 
